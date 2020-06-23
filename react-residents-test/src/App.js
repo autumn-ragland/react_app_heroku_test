6938ad7d149186630d6b55a3d7a9cf535491dd8c
autumn-ragland <raglanda998@gmail.com>
2020-06-23 11:07:45 -0500

    create simple react app

---
+++ react-residents-test/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Residents from './components/Residents'
+
+function App() {
+  return (
+    <div className="App">
+      <Residents/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-residents-test/src/components/Residents.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Residents extends Component {
+  constructor(props){
+      super(props);
+  };
+  render() {
+      return (
+          <div>
+              <h4>Resident Registry!</h4>
+          </div>
+      );
+  }
+}
+
+export default Residents;
