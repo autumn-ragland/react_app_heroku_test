63b66aba92a35ed5e004a29a6fb4d3e7ed8a31c1
autumn-ragland <raglanda998@gmail.com>
2020-07-05 14:30:19 -0500

    create express server

---
+++ react-residents-test/build/precache-manifest.fb130e39c1bf38e27d49d216025f8605.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f0299330c7371f0a2e79c42b0cebdc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7840c17f746ad52894c5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "153bfdcc3bf85e666942",
+    "url": "/static/js/2.c952adcf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c952adcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7840c17f746ad52894c5",
+    "url": "/static/js/main.af892539.chunk.js"
+  },
+  {
+    "revision": "77529cdea5b02a148f13",
+    "url": "/static/js/runtime-main.14d5bc94.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
